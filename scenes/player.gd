77f2e495b560b74901d7e70e9f4a12d84b9bcb3b
Michael Lott <michaellott112358@gmail.com>
2025-12-16 11:30:33 -0500

    d
    d

--- scenes/player.gd
+++ scenes/player.gd
@@ -10,6 +10,9 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if is_on_floor():
+		tile
+
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
