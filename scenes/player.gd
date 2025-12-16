16881c8350214a85326744bbb132b7d3c54e47f5
Michael Lott <michaellott112358@gmail.com>
2025-12-15 23:08:14 -0500

    Player added
    added the kangeroo player sprite
    set up collision for the tile map
    set up collision for the player

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,31 +1,25 @@
-extends Sprite2D
+extends CharacterBody2D
 
-var y_velocity = 400
-var x_velocity = 400
-var gravity = 200
-var jumpHeight = -250
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var playerDirection = 0
-	if Input.is_action_pressed("Right") :
-		playerDirection = 1
-	if Input.is_action_pressed("Left") :
-		playerDirection = -1 
-	if Input.is_action_just_pressed("Jump") and position.y > 550: 
-		y_velocity = jumpHeight
-	if position.y < 630 : 
-		y_velocity += gravity * delta
-	if position.x > 1100 : 
-		position.x = 1100
-	if position.x < 60 : 
-		position.x = 60
 
-	print(position.x, position.y)
-	position.x += x_velocity * playerDirection * delta
-	if(position.y < 600 || y_velocity < 0) :
-		position.y += y_velocity * delta
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
